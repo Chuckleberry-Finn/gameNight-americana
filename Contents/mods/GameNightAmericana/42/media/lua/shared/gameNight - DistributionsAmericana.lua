5a7c3af817bf71bd07561b2fbbc2dfb8821a28a4
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-02-01 19:00:44 -0500

    Update gameNight - DistributionsAmericana.lua

--- Contents/mods/GameNightAmericana/42/media/lua/shared/gameNight - DistributionsAmericana.lua
+++ Contents/mods/GameNightAmericana/42/media/lua/shared/gameNight - DistributionsAmericana.lua
@@ -4,5 +4,5 @@ local gameNightDistro = require "gameNight - Distributions"
 gameNightDistro.proceduralDistGameNight.itemsToAdd["AmericanaBox"] = {}
 gameNightDistro.gameNightBoxes["AmericanaBox"] = {
     AmericanaTiles = 1, AmericanaStarter = 1, Americana_Scorecard = 1,
-    RedMeeple = 8, BlackMeeple = 8, YellowMeeple = 8, BlueMeeple = 8, GreenMeeple = 81
+    RedMeeple = 8, BlackMeeple = 8, YellowMeeple = 8, BlueMeeple = 8, GreenMeeple = 8,
 }
